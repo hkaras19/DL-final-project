1fb280201ee8acd377d79acc14fec4ee82768231
Hunter Karas <hkaras19@punahou.edu>
2020-11-18 20:51:14 -0500

    made create-labels

---
+++ code/create-labels.py
@@ -0,0 +1,22 @@
+import os
+import csv
+
+def save_data(filename, labels):
+
+    for label in labels:
+        folder_path = '../data/{}'.format(label)
+        images = os.listdir(folder_path)
+
+        with open(filename, 'a') as csvfile:  
+            csvwriter = csv.writer(csvfile)
+            
+            for image in images:
+                row = [image, label]
+                csvwriter.writerow(row)
+
+if __name__ == '__main__':
+    labels = os.listdir('../data')
+    filename = '../data-1.csv'
+    save_data(filename, labels)
+    
+
