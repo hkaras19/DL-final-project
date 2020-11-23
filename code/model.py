47272a3b1c8ea1971cd6df37f112ed8f8f18ce4b
arun-kav <70228612+arun-kav@users.noreply.github.com>
2020-11-22 20:47:54 -0500

    add insta_predict

--- code/model.py
+++ code/model.py
@@ -17,9 +17,12 @@ class Model(tf.keras.Model):
         self.batch_size = batch_size
         self.num_classes = num_classes
         
+        self.label_names = ["turn left", "turn right", "jump", "slide", "lean left", "lean right"]
+        
         # feature_extractor_model = "https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/4"
         # base_layer = hub.KerasLayer(feature_extractor_model, input_shape=(224, 224, 3), trainable=False)
 
+    # Create the model
     def create_model(image_shape):
         # Function to create the transfer-learned model used for training.
         # Inputs: train_data, test_data - the data used for training and testing - # Batch_size, x, y, z
@@ -38,12 +41,35 @@ class Model(tf.keras.Model):
         #Make the model
         return tf.keras.Sequential([base_model, pool_layer, out_layer])
 
+
+    
+# Train the model
 def train(self, image_shape, epochs, train_data, train_labels):
     # Compile the model
     self.create_model(image_shape).compile(optimizer=tf.keras.optimizers.Adam(lr=base_learning_rate), loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
                    
     # And train it
     return self.model.fit(train_data, train_labels, epochs = epochs);
+    
+# Predict the output of a single image (or a bunch) in a stream
+def insta_predict(self, test_img, labels):
+    # For larger batches
+    #self.model.predict(test_img);
+    
+    # Make sure the test_img is 4-D
+    try:
+        x = test_img.shape[3]
+    except:
+        x = tf.expand_dims(test_img, axis=0);
+    
+    # For a single batch
+    preds = self.model(x, training=False);
+    
+    labels = []
+    for i in preds:
+        labels.append(self.label_names(tf.math.argmax(i))
+                      
+    return labels
     
 def main():
     # Get the data
@@ -60,6 +86,9 @@ def main():
     # train the model
     print("\nStarting Training...")
     model = train(self, image_shape, epochs, train_data, train_labels)
+    
+    # Call insta_predict -> returns a 1-D list of the labels of the images put in
+    # insta_predict(self, test_img)
     
     # See the test results
     # test_loss, test_accuracy = model.evaluate(test_images, test_labels, verbose=2, batch_size=batch_size)
