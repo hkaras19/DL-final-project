63fd0efb632670f6180838fb35b2681dc9fb5a70
Arun Kavishwar <arun_kavishwar@brown.edu>
2020-12-05 21:20:19 -0500

    87% on 2.5k images

--- renaming_file.py
+++ renaming_file.py
@@ -3,56 +3,86 @@ import os
 # Basic file to rename the data to proper names
 def main():
 
-    # folder = "../../../Desktop/TEMPLEFLOW_PICS" #For my personal computer
-    folder = "./data/train" # For the github
-
+    folder = "../../../Desktop/templeflow_pics_3/train" #For my personal computer
+    #folder = "./data/train" # For the github
+    
+    i = 0
+    j = 0
     # turn_left
     cur_fold = os.path.join(folder, "turn_left")
     for count, filename in enumerate(os.listdir(cur_fold)):
-        dst = "turn_left" + str(count) + ".jpeg"
+        dst = "turn_left" + str(count) + ".jpg"
         src = os.path.join(cur_fold, filename)
         dst = os.path.join(cur_fold, dst)
         os.rename(src, dst);
-
+        i += 1
+        j += 1
+    
+    print(str(i) + " pics in turn_left");
+    i = 0
     # turn_right
     cur_fold = os.path.join(folder, "turn_right")
     for count, filename in enumerate(os.listdir(cur_fold)):
-        dst = "turn_right" + str(count) + ".jpeg"
+        dst = "turn_right" + str(count) + ".jpg"
         src = os.path.join(cur_fold, filename)
         dst = os.path.join(cur_fold, dst)
         os.rename(src, dst);
-        
+        i += 1
+        j += 1
+    
+    print(str(i) + " pics in turn_right");
+    i = 0
     # lean_left
     cur_fold = os.path.join(folder, "lean_left")
     for count, filename in enumerate(os.listdir(cur_fold)):
-        dst = "lean_left" + str(count) + ".jpeg"
+        dst = "lean_left" + str(count) + ".jpg"
         src = os.path.join(cur_fold, filename)
         dst = os.path.join(cur_fold, dst)
         os.rename(src, dst);
+        i += 1
+        j += 1
+        
+    print(str(i) + " pics in lean_left");
+    i = 0
         
     #lean_right
     cur_fold = os.path.join(folder, "lean_right")
     for count, filename in enumerate(os.listdir(cur_fold)):
-        dst = "lean_right" + str(count) + ".jpeg"
+        dst = "lean_right" + str(count) + ".jpg"
         src = os.path.join(cur_fold, filename)
         dst = os.path.join(cur_fold, dst)
         os.rename(src, dst);
+        i += 1
+        j += 1
         
+    print(str(i) + " pics in lean_right");
+    i = 0
+    
     # slide
     cur_fold = os.path.join(folder, "slide")
     for count, filename in enumerate(os.listdir(cur_fold)):
-        dst = "slide" + str(count) + ".jpeg"
+        dst = "slide" + str(count) + ".jpg"
         src = os.path.join(cur_fold, filename)
         dst = os.path.join(cur_fold, dst)
         os.rename(src, dst);
+        i += 1
+        j += 1
+        
+    print(str(i) + " pics in slide");
+    i = 0
         
     # Jump
     cur_fold = os.path.join(folder, "jmp")
     for count, filename in enumerate(os.listdir(cur_fold)):
-        dst = "jmp" + str(count) + ".jpeg"
+        dst = "jmp" + str(count) + ".jpg"
         src = os.path.join(cur_fold, filename)
         dst = os.path.join(cur_fold, dst)
         os.rename(src, dst);
+        i += 1
+        j += 1
+        
+    print(str(i) + " pics in jmp");
+    print(str(j) + " pics total")
         
 # Driver Code
 if __name__ == '__main__':
